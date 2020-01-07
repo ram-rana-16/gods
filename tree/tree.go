5f4a28e00fb652ad436f4721a7f2809ca3e777e3
RB <rb.rana1993@gmail.com>
2020-01-07 23:30:43 +0530

    delete node from binary tree

--- tree/tree.go
+++ tree/tree.go
@@ -32,3 +32,39 @@ func (t *Tree)Insert(v int) *Tree{
 	}
 	return t
 }
+
+// Delete deletes node from tree 
+func (t *Tree)Delete(v int) *Tree{
+	if t == nil {
+		return t
+	}
+	fmt.Println("node ", t)
+	if t.Value > v {
+		t.L = t.L.Delete(v)
+	}else if t.Value < v {
+		t.R = t.R.Delete(v)
+	}else if t.L == nil && t.R == nil {
+		fmt.Println("case 1")
+		t = nil
+	}else if t.L == nil {  // case 2: one child
+		fmt.Println("case 2")
+		t = t.R
+	}else if t.R == nil {
+		t = t.L
+	}else { // case 3: 3 child 
+		minNode := findMin(t.R)
+		t.Value = minNode.Value
+		t.R = t.R.Delete(minNode.Value)
+	}
+	fmt.Println("returned t", t)
+	return t
+
+}
+
+
+func findMin(t *Tree) *Tree {
+	for t.L != nil {
+		t = t.L
+	}
+	return t
+}
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -2,10 +2,21 @@ package tree
 
 import(
 	"testing"
+	"fmt"
 )
 
 func TestInsert(t *testing.T) {
 	tree := &Tree{Value:6}
 	tree = tree.Insert(8).Insert(9).Insert(2)
+	tree.Print()
+}
+
+func TestDelete(t *testing.T) {
+	tree := &Tree{Value:12}
+	tree.Insert(5).Insert(17).Insert(3).Insert(7).Insert(13).
+	Insert(1).Insert(9).Insert(14).Insert(20).Insert(8).Insert(18)
+	tree.Print()
+	fmt.Println("after deletion")
+	tree.Delete(17)
 	tree.Print()
 }
