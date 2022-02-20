7962ce5521da406124f52da4f69f3ca31e96c154
Ram Rana <rb.rana1993@gmail.com>
2022-02-20 19:03:59 +0530

    two sum array

---
+++ array/twosum.go
@@ -0,0 +1,20 @@
+package main
+
+
+// https://leetcode.com/problems/two-sum/
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+func twoSum(nums []int, target int) []int {
+	mymap := make(map[int]int, len(nums))
+	for i, num := range nums {
+		if pos, ok := mymap[target - num]; ok {
+			return []int{pos, i}
+		}else {
+			mymap[num] = i  
+		}
+	}
+	return nil 
+}
+
+
